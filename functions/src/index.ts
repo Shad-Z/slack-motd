8537dccb39bc152343661f6d946b232fef5ad4e7
lepiaf <1940947+lepiaf@users.noreply.github.com>
2022-10-04 12:13:43 +0200

    feat: promisify slack request

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,13 @@
 import * as functions from "firebase-functions";
-import * as https from "https";
+import * as slack from './slack';
+import * as config from './config';
 import * as admin from "firebase-admin";
 import * as util from "util";
-
-const SLACK_TOKEN = functions.config().slack?.token || process.env.SLACK_TOKEN;
-const SLACK_CHANNEL = functions.config().slack?.channel ||
-    process.env.SLACK_CHANNEL ||
-    "meme-of-the-day-game";
-const FIREBASE_STORAGE_BUCKET_NAME = functions.config().storage?.bucketname ||
-    process.env.FIREBASE_STORAGE_BUCKET_NAME;
-const FIREBASE_STORAGE_BASE_URL = "https://firebasestorage.googleapis.com/v0/b/%s/o/%s?alt=media&token=%s";
 
 admin.initializeApp();
 
 const postMeme = async (callback: { (jsonBody: string): void; } | null) => {
+  functions.logger.info("Start function postMeme");
   const memeUrls: string[] = [];
   functions.logger.info("Start function postMeme");
   const files = await admin.storage()
@@ -23,50 +17,15 @@ const postMeme = async (callback: { (jsonBody: string): void; } | null) => {
   for (const f of files[0]) {
     const fileMetadata = await f.getMetadata();
     const url = util.format(
-        FIREBASE_STORAGE_BASE_URL,
-        FIREBASE_STORAGE_BUCKET_NAME,
+        config.FIREBASE_STORAGE_BASE_URL,
+        config.FIREBASE_STORAGE_BUCKET_NAME,
         encodeURI(fileMetadata[0].name),
         fileMetadata[0].metadata.firebaseStorageDownloadTokens,
     );
     memeUrls.push(url);
   }
-  const memeUrl = memeUrls[Math.floor(Math.random() * memeUrls.length)];
-  const message = {
-    channel: SLACK_CHANNEL,
-    text: memeUrl,
-  };
-  const url = "https://slack.com/api/chat.postMessage";
-  const options = {
-    headers: {
-      "Authorization": "Bearer " + SLACK_TOKEN,
-      "Content-Type": "application/json; charset=utf-8",
-    },
-    method: "POST",
-  };
-  const slackReq = https.request(url, options, (slackRes) => {
-    functions.logger.info("Prepare posting meme to slack channel");
-    let body = "";
-    slackRes.on("data", (data: string) => {
-      functions.logger.debug("Read response from slack");
-      body += data;
-    });
-    slackRes.on("end", () => {
-      const jsonBody = JSON.parse(body);
-      const logResponseAs = jsonBody.ok ?
-          functions.logger.info :
-          functions.logger.error;
-      logResponseAs(
-          "Output slack response",
-          {ok: jsonBody.ok, error: jsonBody?.error}
-      );
-      if (callback) {
-        callback(body);
-      }
-    });
-  });
-  functions.logger.info("Posting meme to slack channel", {payload: message});
-  slackReq.write(JSON.stringify(message));
-  slackReq.end();
+  const memeUrl = memeUrls[Math.floor(Math.random() * memeUrls.length)] ?? "no meme found in storage";
+  await slack.postToSlack(memeUrl, callback);
 };
 
 export const fnPostMeme = functions
@@ -86,15 +45,5 @@ exports.scheduledFunction = functions
     .timeZone("Europe/Paris")
     .onRun(() => {
       postMeme(null);
-      return null;
-    });
-
-exports.scheduledPostResult = functions
-    .region("europe-west1")
-    .pubsub
-    .schedule("every tuesday 17:00")
-    .timeZone("Europe/Paris")
-    .onRun(() => {
-      functions.logger.info("Post result")
       return null;
     });
