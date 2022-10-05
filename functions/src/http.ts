93f1d1609808b45256fdd2e7f216344948a013de
lepiaf <1940947+lepiaf@users.noreply.github.com>
2022-10-05 21:30:31 +0200

    create http client for slack

---
+++ functions/src/http.ts
@@ -0,0 +1,44 @@
+import * as functions from "firebase-functions";
+import * as https from "https";
+import * as http from "http";
+import * as appConfig from "./config";
+
+const createRequest = (url: string, method: string, jsonContent = ""): Promise<http.IncomingMessage> => {
+  return new Promise((resolve) => {
+    const options = {
+      headers: {
+        "Authorization": "Bearer " + appConfig.SLACK_TOKEN,
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      method: method,
+    };
+    const req = https.request(url, options, resolve);
+    if (jsonContent) {
+      req.write(jsonContent);
+    }
+    req.end();
+  });
+};
+
+const responseReader = (httpResponse: http.IncomingMessage): Promise<string> => {
+  return new Promise((resolve) => {
+    let body = "";
+    httpResponse.on("data", (data: string) => {
+      body += data;
+    });
+
+    httpResponse.on("end", () => {
+      const jsonBody = JSON.parse(body);
+      const logResponseAs = jsonBody.ok ?
+          functions.logger.info :
+          functions.logger.error;
+      logResponseAs(
+          "Output slack response",
+          {ok: jsonBody.ok, error: jsonBody?.error}
+      );
+      resolve(jsonBody);
+    });
+  });
+};
+
+export {createRequest, responseReader};
--- functions/src/slack.ts
+++ functions/src/slack.ts
@@ -1,56 +1,33 @@
-import * as functions from "firebase-functions";
-import * as https from "https";
-import * as http from "http";
 import * as appConfig from "./config";
+import {createRequest, responseReader} from "./http";
 
-const postToSlack = async (text: string, callback: { (jsonBody: string): void; } | null) => {
-  const slackReq = (text: string): Promise<http.IncomingMessage> => {
-    return new Promise((resolve) => {
-      functions.logger.info("Prepare posting meme to slack channel");
-      const message = {
-        channel: appConfig.SLACK_CHANNEL,
-        text: text,
-      };
-      const url = "https://slack.com/api/chat.postMessage";
-      const options = {
-        headers: {
-          "Authorization": "Bearer " + appConfig.SLACK_TOKEN,
-          "Content-Type": "application/json; charset=utf-8",
-        },
-        method: "POST",
-      };
-      const req = https.request(url, options, resolve);
-      req.write(JSON.stringify(message));
-      req.end();
-      functions.logger.info("Posting meme to slack channel", {payload: message});
-    });
+const postToSlack = async (text: string): Promise<string> => {
+  const message = {
+    channel: appConfig.SLACK_CHANNEL,
+    text: text,
   };
-  const promiseRes = (slackRes: http.IncomingMessage): Promise<string> => {
-    return new Promise((res) => {
-      let body = "";
-      slackRes.on("data", (data: string) => {
-        functions.logger.debug("Read response from slack");
-        body += data;
-      });
-      slackRes.on("end", () => {
-        const jsonBody = JSON.parse(body);
-        const logResponseAs = jsonBody.ok ?
-            functions.logger.info :
-            functions.logger.error;
-        logResponseAs(
-            "Output slack response",
-            {ok: jsonBody.ok, error: jsonBody?.error}
-        );
-        res(jsonBody);
-      });
-    });
-  };
-
-  const slackRes = await slackReq(text);
-  const jsonBody = await promiseRes(slackRes);
-  if (callback) {
-    callback(jsonBody);
-  }
+  const slackRes = await createRequest(
+      "https://slack.com/api/chat.postMessage",
+      "POST",
+      JSON.stringify(message),
+  );
+  return await responseReader(slackRes);
 };
 
-export {postToSlack};
+const getHistories = async () => {
+  const slackRes = await createRequest(
+      `https://slack.com/api/conversations.history?channel=${appConfig.SLACK_CHANNEL}&limit=1`,
+      "GET",
+  );
+  return await responseReader(slackRes);
+};
+
+const getReplies = async (tsLastMessage: string) => {
+  const slackRes = await createRequest(
+      `https://slack.com/api/conversations.replies?channel=${appConfig.SLACK_CHANNEL}&ts=${tsLastMessage}`,
+      "GET",
+  );
+  return await responseReader(slackRes);
+};
+
+export {postToSlack, getHistories, getReplies};
