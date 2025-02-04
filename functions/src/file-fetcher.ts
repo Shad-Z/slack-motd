bd628e20b6df21549b1bbe6a537e2940308cfc92
lepiaf <1940947+lepiaf@users.noreply.github.com>
2025-02-04 22:56:16 +0100

    feat: upgrade function v2

--- functions/src/file-fetcher.ts
+++ functions/src/file-fetcher.ts
@@ -4,19 +4,22 @@ import * as util from "util";
 
 export const getMemeUrls = async (): Promise<string[]> => {
   const files = await admin.storage()
-      .bucket()
-      .getFiles();
+    .bucket()
+    .getFiles();
 
   return Promise.all(
-      files[0].map(async (f) => {
-        const fileMetadata = await f.getMetadata();
-
-        return util.format(
-            "https://firebasestorage.googleapis.com/v0/b/%s/o/%s?alt=media&token=%s",
-            config.STORAGE_BUCKET_NAME,
-            encodeURI(fileMetadata[0].name),
-            fileMetadata[0].metadata.firebaseStorageDownloadTokens,
-        );
-      })
+    files[0].map(async (f) => {
+      const fileMetadata = await f.getMetadata();
+      const file = fileMetadata[0];
+      if (!file || !file.name || !file.metadata) {
+        return "";
+      }
+      return util.format(
+        "https://firebasestorage.googleapis.com/v0/b/%s/o/%s?alt=media&token=%s",
+        config.STORAGE_BUCKET_NAME,
+        encodeURI(file.name),
+        file.metadata.firebaseStorageDownloadTokens,
+      );
+    })
   );
 };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,27 +1,26 @@
-import * as functions from "firebase-functions";
-import * as slack from "./slack";
-import * as admin from "firebase-admin";
-import * as memeChooser from "./meme-chooser";
-import {gather} from "./gather";
-import {httpWebhookController} from "./http-webhook-controller";
+import {onSchedule} from "firebase-functions/v2/scheduler";
 import {getFirestore} from "firebase-admin/firestore";
+import {chooseMeme} from "./meme-chooser";
+import admin from "firebase-admin";
+import {postToSlack} from "./slack";
+import {gather} from "./gather";
+import {logger} from "firebase-functions";
+import {onRequest} from "firebase-functions/v2/https";
+import {httpWebhookController} from "./http-webhook-controller";
 
 admin.initializeApp();
+
 const db = getFirestore();
 
-exports.httpWebhook = functions
-    .region("europe-west1")
-    .https
-    .onRequest(httpWebhookController);
+exports.httpWebhook = onRequest(
+  {region: "europe-west1"},
+  httpWebhookController
+);
 
-exports.scheduledFunction = functions
-    .region("europe-west1")
-    .pubsub
-    .schedule("every tuesday 09:00")
-    .timeZone("Europe/Paris")
-    .onRun(async () => {
-      const meme = await memeChooser.chooseMeme();
-      const msg = `<!here> ${meme}
+
+exports.scheduledFunction = onSchedule({schedule: "every tuesday 09:00", timeZone: "Europe/Paris", region: "europe-west1"}, async () => {
+  const meme = await chooseMeme();
+  const msg = `<!here> ${meme}
 Rappel des règles :
 
 Tous les mardis, un meme est posté à 9h du matin dans #meme-of-the-week
@@ -32,44 +31,28 @@ Le joueur ayant reçu le plus de :white_check_mark: à la fin de la journée gag
 
 Vous pouvez aussi voter pour la pire phrase avec un beau :hankey:, les perdants seront bien entendu shame comme il se doit !
 `;
-      const response = await slack.postToSlack(msg);
-      const docRef = db.collection("slack").doc("latest");
-      await docRef.set(response);
+  const response = await postToSlack(msg);
+  const docRef = db.collection("slack").doc("latest");
+  await docRef.set(response);
+});
 
-      return null;
-    });
+exports.scheduledPostResult = onSchedule({schedule: "every tuesday 18:30", timeZone: "Europe/Paris", region: "europe-west1"}, async () => {
+  const docRef = db.collection("slack").doc("latest");
+  const doc = await docRef.get();
+  if (!doc.exists) {
+    logger.error("No such document!");
+    return;
+  }
+  const docData = doc.data();
+  const tsLastMessage = docData?.message?.ts;
+  if (!tsLastMessage) {
+    logger.error("ts not found", docData);
+    return;
+  }
 
-exports.scheduledPostResult = functions
-    .region("europe-west1")
-    .pubsub
-    .schedule("every tuesday 18:30")
-    .timeZone("Europe/Paris")
-    .onRun(async () => {
-      const docRef = db.collection("slack").doc("latest");
-      const doc = await docRef.get();
-      if (!doc.exists) {
-        functions.logger.error("No such document!");
-        return null;
-      }
-      const docData = doc.data();
-      const tsLastMessage = docData?.message?.ts;
-      if (!tsLastMessage) {
-        functions.logger.error("ts not found", docData);
-        return null;
-      }
+  await gather(tsLastMessage);
+});
 
-      await gather(tsLastMessage);
-
-      return null;
-    });
-
-exports.scheduledRappelVote = functions
-    .region("europe-west1")
-    .pubsub
-    .schedule("every tuesday 17:30")
-    .timeZone("Europe/Paris")
-    .onRun(async () => {
-      await slack.postToSlack("<!here> Hop hop hop ! On oublie pas de voter pour Mr/Mme FUN :) Résultat à 18h30 !");
-
-      return null;
-    });
+exports.scheduledRappelVote = onSchedule({schedule: "every tuesday 17:30", timeZone: "Europe/Paris", region: "europe-west1"}, async () => {
+  await postToSlack("<!here> Hop hop hop ! On oublie pas de voter pour Mr/Mme FUN :) Résultat à 18h30 !");
+});
